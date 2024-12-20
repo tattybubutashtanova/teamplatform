8d1c3bb403632390ef0352b5b673a9498bd7092a
Tattybubu Tashtanova <tattybubu.tashtanova_2027@ucentralasia.org>
2024-12-20 10:59:22 +0600

    Initial commit: Set up Spring Boot project

---
+++ src/main/java/com/teamcollab/teamplatform/TeamplatformApplication.java
@@ -0,0 +1,13 @@
+package com.teamcollab.teamplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamcollab/teamplatform/TeamplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcollab.teamplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
